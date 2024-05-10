3abf07ac4d530979b48cfcd3ff129a455320ff43
Tim Pansino <timpansino@gmail.com>
2024-05-09 17:20:06 -0700

    Fixing flask app

--- .github/containers/python/main.py
+++ .github/containers/python/main.py
@@ -1,13 +1,10 @@
-from flask import Flask, abort
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    try:
-        from newrelic.agent import current_transaction
-        assert current_transaction(), "No active transaction."
-    except Exception:
-        abort(417)
+    from newrelic.agent import current_transaction
+    assert current_transaction(), "No active transaction."
     
     return "<p>Hello, World!</p>"
