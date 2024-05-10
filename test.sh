94da0fcb6f718ea3a2bbedb8d1fadc250849b873
Tim Pansino <timpansino@gmail.com>
2024-05-10 11:23:33 -0700

    TEMP COMMIT

--- test.sh
+++ test.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-docker build -t e2e/initcontainer-python:e2e python/
-minikube image load e2e/initcontainer-python:e2e
-
-docker build -t e2e/python-test-app:e2e .github/containers/python/
-minikube image load e2e/python-test-app:e2e
+minikube image build -t e2e/initcontainer-python:e2e python/
+minikube image build -t e2e/python-test-app:e2e .github/containers/python/
 
 kubectl delete -f .github/containers/python/customresource.yaml -n python-test
 kubectl apply -f .github/containers/python/customresource.yaml -n python-test
@@ -14,7 +11,8 @@ kubectl apply -f .github/containers/python/python_test_app_deployment.yaml -n py
 
 kubectl get pods --namespace=python-test
 
-# kubectl logs -n python-test $(kubectl get pods --namespace=python-test | grep python-test | cut -d" " -f1)
-
 kubectl wait --for=condition=Ready --namespace python-test --all pods
 minikube service python-test-app-service --url --namespace python-test
+
+
+# kubectl logs -n python-test $(kubectl get pods --namespace=python-test | grep python-test | cut -d" " -f1)
