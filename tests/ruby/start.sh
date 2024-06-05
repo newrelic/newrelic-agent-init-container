99ab3ac5370f5980cdffb9c74523b0a324ecbc35
Hannah Ramadan <hannahr315@gmail.com>
2024-06-05 13:55:38 -0700

    Hit endpoint

--- tests/ruby/start.sh
+++ tests/ruby/start.sh
@@ -3,8 +3,8 @@
 # Start ruby app and provide time to boot Puma
 ruby app.rb & sleep 5
 
-# For local testing - hit endpoint
-# while true; do
-#   curl http://0.0.0.0:4567
-#   sleep 3
-# done
+# Hit endponint
+while true; do
+  curl http://0.0.0.0:4567
+  sleep 3
+done
