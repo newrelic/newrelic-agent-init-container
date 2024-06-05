b6209370f29e08179f1be90a979efff06f453c5a
Hannah Ramadan <hannahr315@gmail.com>
2024-06-05 09:46:11 -0700

    Let CI handle making requests

--- tests/ruby/start.sh
+++ tests/ruby/start.sh
@@ -3,8 +3,8 @@
 # Start ruby app and provide time to boot Puma
 ruby app.rb & sleep 5
 
-# Make requests
-while true; do
-  curl http://0.0.0.0:4567
-  sleep 3
-done
+# Make requests - for local testing
+# while true; do
+#   curl http://0.0.0.0:4567
+#   sleep 3
+# done
