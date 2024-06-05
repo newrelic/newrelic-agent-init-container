2afefe11a55180ea33b1b54ec12ea1963f432f30
Hannah Ramadan <hannahr315@gmail.com>
2024-06-05 13:43:33 -0700

    Add publish workflow

--- tests/ruby/start.sh
+++ tests/ruby/start.sh
@@ -3,8 +3,8 @@
 # Start ruby app and provide time to boot Puma
 ruby app.rb & sleep 5
 
-# Make requests
-while true; do
-  curl http://0.0.0.0:4567
-  sleep 3
-done
+# For local testing - hit endpoint
+# while true; do
+#   curl http://0.0.0.0:4567
+#   sleep 3
+# done
