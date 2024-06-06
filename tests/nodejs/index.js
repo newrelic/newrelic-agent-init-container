3c31c8aad96d3a604d95ce3924a78ac05087c2dd
mrickard <maurice@mauricerickard.com>
2024-06-06 15:51:57 -0400

    test: Restored reference to secrets: inherit. Unable to access *any* secrets when PRing from fork
    Signed-off-by: mrickard <maurice@mauricerickard.com>
    
    Signed-off-by: mrickard <maurice@mauricerickard.com>

--- tests/nodejs/index.js
+++ tests/nodejs/index.js
@@ -2,45 +2,8 @@ const http = require('http')
 
 const PAYLOAD = JSON.stringify({ msg: 'ok' })
 
-const PAGE =
-    '<html>' +
-    '<head><title>test response</title></head>' +
-    '<body><p>I heard you like HTML.</p></body>' +
-    '</html>'
-
-
-function makeRequest(params, cb) {
-    const req = http.request(params, function (res) {
-        if (res.statusCode !== 200) {
-            return cb(null, res.statusCode, null)
-        }
-
-        res.setEncoding('utf8')
-        res.on('data', function (data) {
-            cb(null, res.statusCode, data)
-        })
-    })
-
-    req.on('error', function (err) {
-        // If we aborted the request and the error is a connection reset, then
-        // all is well with the world. Otherwise, ERROR!
-        if (params.abort && err.code === 'ECONNRESET') {
-            cb()
-        } else {
-            cb(err)
-        }
-    })
-
-    if (params.abort) {
-        setTimeout(function () {
-            req.abort()
-        }, params.abort)
-    }
-    req.end()
-}
-
 function startServer () {
-    const external = http.createServer(function (request, response) {
+    const server = http.createServer(function (request, response) {
         response.writeHead(200, {
             'Content-Length': PAYLOAD.length,
             'Content-Type': 'application/json'
@@ -48,41 +11,11 @@ function startServer () {
         response.end(PAYLOAD)
     })
 
-    const server = http.createServer(function (request, response) {
-        if (/\/slow$/.test(request.url)) {
-            setTimeout(function () {
-                response.writeHead(200, {
-                    'Content-Length': PAGE.length,
-                    'Content-Type': 'text/html'
-                })
-                response.end(PAGE)
-            }, 500)
-            return
-        }
-        makeRequest(
-            {
-                port: 8000,
-                host: 'localhost',
-                path: '/status',
-                method: 'GET'
-            },
-            function () {
-                response.writeHead(200, {
-                    'Content-Length': PAGE.length,
-                    'Content-Type': 'text/html'
-                })
-                response.end(PAGE)
-            }
-        )
-    })
-
     return new Promise((resolve) => {
-        external.listen(8000, 'localhost', function () {
-            server.listen(8001, 'localhost', function () {
-                // The transaction doesn't get created until after the instrumented
-                // server handler fires.
-                resolve()
-            })
+        server.listen(8000, 'localhost', function () {
+            // The transaction doesn't get created until after the instrumented
+            // server handler fires.
+            resolve()
         })
     })
 }
