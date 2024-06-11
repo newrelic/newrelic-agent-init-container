4b5a8bf79ed1bdbbc4f3bc00ce6f7853451379fc
Tim Pansino <timpansino@gmail.com>
2024-06-11 14:13:44 -0700

    Fix PHP tests

--- src/php/php-agent-download.sh
+++ src/php/php-agent-download.sh
@@ -1,27 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
 libc=$1
 version=$2
 
-if [ -z "$libc" ]; then
-  echo "'libc' argument not supplied. Defaulting to 'gnu'"
-  libc="gnu"
-elif [[ ! $libc =~ ^(musl|gnu)$ ]]; then
+if [[ -z "$libc" ]]; then
+  echo "'libc' argument not supplied. Defaulting to 'glibc'"
+  libc="glibc"
+elif [[ "$libc" != "glibc" && "$libc" != "musl" ]]; then
   echo "error: invalid 'libc' argument provided. Exiting."
   exit 1
 fi
 
-if [ -z "$version" ]; then
-  latest=$(curl https://download.newrelic.com/php_agent/release/ | grep newrelic-php5 | cut -d '-' -f 3 | head -n 1)
+if [[ -z "$version" ]]; then
+  latest=$(wget https://download.newrelic.com/php_agent/release/ -O - | grep newrelic-php5 | cut -d '-' -f 3 | head -n 1)
   url="https://download.newrelic.com/php_agent/release/newrelic-php5-${latest}-linux"
 else
   url="https://download.newrelic.com/php_agent/archive/${version}/newrelic-php5-${version}-linux"
 fi
 
 if [ "$libc" = 'musl' ]; then
-  url+="-musl.tar.gz"
+  url="${url}-musl.tar.gz"
 else
-  url+=".tar.gz"
+  url="${url}.tar.gz"
 fi
 
 # echo "url: $url"
