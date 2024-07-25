1f1c37684d07e722bfb62ca95b7e14765e3163b2
Timothy Pansino <11214426+TimPansino@users.noreply.github.com>
2024-07-25 15:38:34 -0700

    Expanded Python Testing (#78)
    * Expand python version testing matrix
    
    * Ensure binary agent installation is used in tests
    
    * Remove pip for python tests
    
    * Expand assertions for python
    
    * Pin pip to last functional version for Python 3.7

--- tests/python/main.py
+++ tests/python/main.py
@@ -8,10 +8,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    # Ensure transaction is active
     try:
         from newrelic.agent import current_transaction
         assert current_transaction(), "No active transaction."
+    
+        # Ensure no external copy of pip is available
+        try:
+            import pip
+        except ImportError:
+            pass
+        else:
+            raise RuntimeError("External copy of pip found at %s, vendored version should be the only available copy." % str(pip.__file__))
+
+        # Ensure binary installation of agent is used
+        from newrelic_k8s_operator import find_supported_newrelic_distribution
+        new_relic_path = find_supported_newrelic_distribution()
+        assert "linux" in new_relic_path, "Operator failed to find binary agent."
+
+        # Return 200 OK response
+        return "<p>Hello, World!</p><br><p>new_relic_path = %s</p>" % str(new_relic_path)
+
     except Exception:
         return "".join(traceback.format_exception(*sys.exc_info())), 417
-
-    return "<p>Hello, World!</p>"
