87fd9cbb43f8a6be92cf6d22b12de11919f9bf2a
Brian Duranleau <bduranleau@newrelic.com>
2024-08-07 15:15:14 -0500

    chore: address shellcheck feedback

--- src/php/nr_env_to_ini.sh
+++ src/php/nr_env_to_ini.sh
@@ -3,12 +3,12 @@
 map_file=/newrelic-instrumentation/nrini.mapping
 output_file=/newrelic-instrumentation/php-agent/ini/newrelic.ini
 
-if [ ! -z "${NEW_RELIC_LICENSE_KEY}" ]; then
+if [ -n "${NEW_RELIC_LICENSE_KEY}" ]; then
   echo "newrelic.license=${NEW_RELIC_LICENSE_KEY}" >>$output_file
 fi
 
 while IFS=':' read -r envvar inivar; do
-  if [ -n "$(printenv $envvar)" ]; then
-    echo "$inivar=$(printenv $envvar)" >>$output_file
+  if [ -n "$(printenv "$envvar")" ]; then
+    echo "$inivar=$(printenv "$envvar")" >>$output_file
   fi
 done <"$map_file"
