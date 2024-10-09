ac43cf20445675347efbe801b4df98ec85632f07
Brian Duranleau <bduranleau@newrelic.com>
2024-10-09 16:46:51 -0500

    add sleep to test app

--- tests/php/index.php
+++ tests/php/index.php
@@ -3,6 +3,7 @@
 if (extension_loaded('newrelic')) {
   echo "Hello world!";
   phpinfo();
+  sleep(2); // make the transaction interesting enough to sample
 } else {
   throw new Exception('Failed to load newrelic extension');
 }
