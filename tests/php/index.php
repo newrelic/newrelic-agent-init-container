35cd0aea4b8c70d100e6d2409a81e6e2d634a302
Brian Duranleau <bduranleau@newrelic.com>
2024-10-09 17:15:45 -0500

    force the agent to sample

--- tests/php/index.php
+++ tests/php/index.php
@@ -3,7 +3,9 @@
 if (extension_loaded('newrelic')) {
   echo "Hello world!";
   phpinfo();
-  sleep(2); // make the transaction interesting enough to sample
+  $linking_md = newrelic_get_linking_metadata();
+  $payload = json_encode($linking_md);
+  sleep(10); // make the transaction interesting enough to sample
 } else {
   throw new Exception('Failed to load newrelic extension');
 }
